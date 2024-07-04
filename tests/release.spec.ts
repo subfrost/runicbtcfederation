1eb319e6eae9bd2531425df3eb446c1dff6f8ae3
hudeintres <nodadfat@gmail.com>
2024-07-04 16:27:56 +0000

    update tests

--- tests/release.spec.ts
+++ tests/release.spec.ts
@@ -6,8 +6,29 @@ import * as path from "node:path";
 import { expect } from "chai";
 //@ts-ignore
 import bitcoinjs = require("bitcoinjs-lib");
+import * as ECPairFactory from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
 import { encodeRunestone } from "@magiceden-oss/runestone-lib";
 import { MetashrewRunes } from "../lib/rpc";
+import { error } from "node:console";
+
+const network = bitcoinjs.networks.bitcoin;
+
+const ECPair = ECPairFactory.ECPairFactory(ecc);
+
+const EMPTY_BUFFER = Buffer.allocUnsafe(0);
+const EMPTY_WITNESS = [];
+
+const keyPair = ECPair.makeRandom({ network });
+const { publicKey, privateKey } = keyPair;
+const { address } = bitcoinjs.payments.p2pkh({ pubkey: publicKey, network });
+
+const TEST_BTC_ADDRESS1 = address;
+const TEST_BTC_ADDRESS2 = "1AdAhGdUgGF6ip7bBcVvuWYuuCxAeonNaK";
+
+const DEBUG_WASM = fs.readFileSync(
+  path.join(__dirname, "..", "build", "debug.wasm"),
+);
 
 const log = (obj: any) => {
   console.log(inspect(obj, false, 10, true));
@@ -63,10 +84,6 @@ const formatKv = (kv: any) => {
   );
 };
 
-const DEBUG_WASM = fs.readFileSync(
-  path.join(__dirname, "..", "build", "debug.wasm"),
-);
-
 const buildProgram = () => {
   const program = new IndexerProgram(
     new Uint8Array(Array.from(DEBUG_WASM)).buffer,
@@ -76,12 +93,6 @@ const buildProgram = () => {
 };
 
 const buildBytes32 = () => Buffer.allocUnsafe(32);
-
-const EMPTY_BUFFER = Buffer.allocUnsafe(0);
-const EMPTY_WITNESS = [];
-
-const TEST_BTC_ADDRESS1 = "16aE44Au1UQ5XqKMUhCMXTX7ZxbmAcQNA1";
-const TEST_BTC_ADDRESS2 = "1AdAhGdUgGF6ip7bBcVvuWYuuCxAeonNaK";
 
 const buildCoinbase = (outputs) => {
   const tx = new bitcoinjs.Transaction();
@@ -112,11 +123,11 @@ const buildTransaction = (ins, outs) => {
   return tx;
 };
 
-const buildCoinbaseToTestAddress = () =>
+const buildCoinbaseToAddress = (address: string) =>
   buildCoinbase([
     {
       script: bitcoinjs.payments.p2pkh({
-        address: TEST_BTC_ADDRESS1,
+        address: address,
         network: bitcoinjs.networks.bitcoin,
       }).output,
       value: 625000000,
@@ -158,6 +169,95 @@ const runesbyaddress = async (
   return result;
 };
 
+const initCompleteBlockWithRuneEtching = (
+  outputs: any,
+  pointer: number,
+  divisibility: number = 8,
+  premineAmount: bigint = 2100000005000000n,
+  runeName: string = "GENESIS•RUNE•FR",
+  symbol: string = "G",
+  block?: bitcoinjs.Block
+): bitcoinjs.Block => {
+  let coinbase;
+  if (block == undefined) {
+    block = buildDefaultBlock()
+    coinbase = buildCoinbaseToAddress(TEST_BTC_ADDRESS1);
+    block.transactions?.push(coinbase);
+  } else {
+    coinbase = block.transactions?.at(0)
+  }
+  const runesGenesis = encodeRunestone({
+    etching: {
+      divisibility: divisibility,
+      premine: premineAmount,
+      runeName: runeName,
+      symbol: symbol,
+    },
+    pointer: pointer,
+  }).encodedRunestone;
+  const transaction = buildTransaction(
+    [
+      {
+        hash: coinbase.getHash(),
+        index: 0,
+        witness: EMPTY_WITNESS,
+        script: EMPTY_BUFFER,
+      },
+    ],
+    [
+      {
+        script: runesGenesis,
+        value: 0,
+      },
+      ...outputs
+    ],
+  );
+  block.transactions?.push(transaction);
+  return block;
+}
+
+const buildCompleteBlockWithRuneTransfer = (
+  inputs: any,
+  outputs: any,
+  edicts: {
+    id: {
+      block: bigint;
+      tx: number;
+    };
+    amount: bigint;
+    output: number;
+  }[] | undefined,
+  pointer: number,
+  block?: bitcoinjs.Block
+): bitcoinjs.Block => {
+  if (block == undefined) {
+    block = buildDefaultBlock()
+    const coinbase = buildCoinbaseToAddress(TEST_BTC_ADDRESS1);
+    block.transactions?.push(coinbase);
+  }
+  const runesTransfer = encodeRunestone({
+    edicts: edicts,
+    pointer: pointer,
+  }).encodedRunestone;
+  const transaction = buildTransaction(
+    [
+      ...inputs,
+    ],
+    [
+      {
+        script: runesTransfer,
+        value: 0,
+      },
+      ...outputs
+    ],
+  );
+
+  const sighash = transaction.hashForWitnessV0(0, p2wpkh.output!, previousOutputValue, bitcoin.Transaction.SIGHASH_ALL);
+  const signature = bitcoinjs.script.signature.encode(keyPair.sign(sighash), bitcoinjs.Transaction.SIGHASH_ALL);
+  block.transactions?.push(transaction);
+  return block;
+}
+
 describe("metashrew-runes", () => {
   it("should check if duplicate keys are not being set", async () => {
     const program = buildProgram();
@@ -173,54 +273,148 @@ describe("metashrew-runes", () => {
       ).length,
     ).to.be.equal(2);
   });
-  it("should index Runestone", async () => {
+  it("should not index before 840000", async () => {
+    const program = buildProgram();
+    program.setBlockHeight(839000);
+    const premineAmount = 2100000005000000n
+    const outputs = [{
+      script: bitcoinjs.payments.p2pkh({
+        address: TEST_BTC_ADDRESS1,
+        network: bitcoinjs.networks.bitcoin,
+      }).output,
+      value: 1,
+    },
+    {
+      script: bitcoinjs.payments.p2pkh({
+        network: bitcoinjs.networks.bitcoin,
+        address: TEST_BTC_ADDRESS2,
+      }).output,
+      value: 624999999,
+    }]
+    const block = initCompleteBlockWithRuneEtching(outputs, 1, undefined, premineAmount)
+    program.setBlock(block.toHex());
+    await program.run("_start");
+
+    const resultAddress1 = await runesbyaddress(program, TEST_BTC_ADDRESS1);
+    expect(resultAddress1.balanceSheet.length == 0);
+
+    const resultAddress2 = await runesbyaddress(program, TEST_BTC_ADDRESS2);
+    expect(resultAddress2.balanceSheet.length == 0);
+  });
+  it("index Runestone on etching and premine", async () => {
     const program = buildProgram();
     program.setBlockHeight(840001);
-    const block = buildDefaultBlock();
-    const coinbase = buildCoinbaseToTestAddress();
-    block.transactions?.push(coinbase);
-    const runesGenesis = encodeRunestone({
-      etching: {
-        divisibility: 8,
-        premine: 2100000000000000n,
-        runeName: "GENESIS•RUNE•FR",
-        symbol: "G",
+    const premineAmount = 2100000005000000n
+    const outputs = [{
+      script: bitcoinjs.payments.p2pkh({
+        address: TEST_BTC_ADDRESS1,
+        network: bitcoinjs.networks.bitcoin,
+      }).output,
+      value: 1,
+    },
+    {
+      script: bitcoinjs.payments.p2pkh({
+        network: bitcoinjs.networks.bitcoin,
+        address: TEST_BTC_ADDRESS2,
+      }).output,
+      value: 624999999,
+    }]
+    const block = initCompleteBlockWithRuneEtching(outputs, 1, undefined, premineAmount)
+    program.setBlock(block.toHex());
+    await program.run("_start");
+
+    const resultAddress1 = await runesbyaddress(program, TEST_BTC_ADDRESS1);
+    expect(resultAddress1.balanceSheet[0].balance == premineAmount);
+
+    const resultAddress2 = await runesbyaddress(program, TEST_BTC_ADDRESS2);
+    expect(resultAddress2.balanceSheet.length == 0);
+  });
+  it("index Runestone on transfer", async () => {
+    const program = buildProgram();
+    program.setBlockHeight(840000);
+    const premineAmount = 2100000005000000n
+    const outputWithRune = {
+      script: bitcoinjs.payments.p2pkh({
+        address: TEST_BTC_ADDRESS1,
+        network: bitcoinjs.networks.bitcoin,
+      }).output,
+      value: 1,
+    }
+    const outputWithoutRune = {
+      script: bitcoinjs.payments.p2pkh({
+        network: bitcoinjs.networks.bitcoin,
+        address: TEST_BTC_ADDRESS1,
+      }).output,
+      value: 624999999,
+    }
+    const outputs = [outputWithRune, outputWithoutRune]
+    const pointer1 = 1
+    const block = initCompleteBlockWithRuneEtching(outputs, pointer1, undefined, premineAmount)
+    const runeGenesisTx = block.transactions?.at(1)
+
+    const inputs = [
+      {
+        hash: runeGenesisTx?.getHash(),
+        index: pointer1,
+        witness: EMPTY_WITNESS,
+        script: EMPTY_BUFFER,
       },
+      {
+        hash: runeGenesisTx?.getHash(),
+        index: 2,
+        witness: EMPTY_WITNESS,
+        script: EMPTY_BUFFER,
+      },
+    ]
+    const outputs2 = [
+      {
+        script: bitcoinjs.payments.p2pkh({
+          address: TEST_BTC_ADDRESS2,
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 1,
+      },
+    ]
+    const edicts = [
+      {
+        id: {
+          block: 840000n,
+          tx: 1
+        },
+        amount: premineAmount,
+        output: 1
+      }
+    ]
+    // const newBlock = buildCompleteBlockWithRuneTransfer(inputs, outputs2, edicts, 1)
+    const runesTransfer = encodeRunestone({
+      edicts: edicts,
       pointer: 1,
     }).encodedRunestone;
     const transaction = buildTransaction(
       [
-        {
-          hash: coinbase.getHash(),
-          index: 0,
-          witness: EMPTY_WITNESS,
-          script: EMPTY_BUFFER,
-        },
+        ...inputs,
       ],
       [
         {
-          script: runesGenesis,
+          script: runesTransfer,
           value: 0,
         },
-        {
-          script: bitcoinjs.payments.p2pkh({
-            address: TEST_BTC_ADDRESS1,
-            network: bitcoinjs.networks.bitcoin,
-          }).output,
-          value: 1,
-        },
-        {
-          script: bitcoinjs.payments.p2pkh({
-            network: bitcoinjs.networks.bitcoin,
-            address: TEST_BTC_ADDRESS1,
-          }).output,
-          value: 624999999,
-        },
+        ...outputs2
       ],
     );
+    const p2wpkh = bitcoinjs.payments.p2wpkh({ pubkey: publicKey });
+    const sighash = transaction.hashForWitnessV0(0, p2wpkh.output!, 1, bitcoinjs.Transaction.SIGHASH_ALL);
+    const signature = bitcoinjs.script.signature.encode(keyPair.sign(sighash), bitcoinjs.Transaction.SIGHASH_ALL);
+    transaction.setWitness(0, [signature, publicKey]);
     block.transactions?.push(transaction);
+
     program.setBlock(block.toHex());
+
     await program.run("_start");
-    console.log(await runesbyaddress(program, TEST_BTC_ADDRESS1));
+
+    const resultAddress1 = await runesbyaddress(program, TEST_BTC_ADDRESS1);
+    expect(resultAddress1.balanceSheet.length === 0);
+    const resultAddress2 = await runesbyaddress(program, TEST_BTC_ADDRESS2);
+    expect(resultAddress2.balanceSheet[0].balance === premineAmount);
   });
 });
