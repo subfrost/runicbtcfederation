82948b95c2302deafa06f2ad25133d574b6ca915
subfrostdev <inquiries@subfrost.io>
2024-08-11 20:15:35 -0400

    use paymentsindex

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,7 @@
 import { _flush, input } from "metashrew-as/assembly/indexer/index";
 import { Block } from "metashrew-as/assembly/blockdata/block";
 import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
-import { SpendablesIndex } from "metashrew-spendables/assembly/indexer";
+import { PaymentsIndex } from "metashrew-payments/assembly/indexer";
 import { FederationIndex } from "./indexer/FederationIndex";
 export * from "protorune/assembly/view";
 import { Box } from "metashrew-as/assembly/utils/box";
@@ -11,7 +11,7 @@ export function _start(): void {
   const data = Box.from(input());
   const height = parsePrimitive<u32>(data);
   const block = new Block(data);
-  new SpendablesIndex().indexBlock(height, block);
+  new PaymentsIndex().indexBlock(height, block);
   new FederationIndex().indexBlock(height, block);
   _flush();
 }
